4affcfb731b4a18e77a24d570fabd6d296081ed3
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-09 16:58:12 -0500

    Auto push

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {}
 
 terraform {
   backend "s3" {
-    bucket = "b58-tf-state-08"
+    bucket = "girisha-b58-tf-state"
     key    = "tools/terraform.tfstate"
     region = "us-east-1"
   }
