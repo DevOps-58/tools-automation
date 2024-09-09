5a8568e8f95813fddea463aa68788532f33b3815
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-09 17:06:12 -0500

    Auto push

--- module/main.tf
+++ module/main.tf
@@ -26,18 +26,6 @@ resource "aws_route53_record" "main" {
   records = [aws_instance.main.private_ip]
 }
 
-resource "null_resource" "app" {
-  depends_on = [aws_route53_record.main, aws_instance.main]
-
-  triggers = {
-    always_run = true
-  }
-
-  provisioner "local-exec" {
-    command = "sleep 10; cd /home/ec2-user/Ansible ; ansible-playbook -i inv-dev  -e ansible_user=ec2-user -e ansible_password=DevOps321 -e COMPONENT=${var.name} -e ENV=dev  expense.yml"
-  
-   }
-}
 
 resource "aws_security_group" "main" {
   name        = var.name
