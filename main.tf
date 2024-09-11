35381e030e3ed6500af0a28fe089373dae03ddb8
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-11 12:55:29 -0500

    Auto push

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "tools" {
   for_each = var.tools
   source   = "./module"
 
-  name          = var.tools["vault"]["name"]
-  instance_type = var.tools["vault"]["instance_type"]
-  port_no       = var.tools["vault"]["port_no"]
+  name          = each.value["name"]
+  instance_type = each.value["instance_type"]
+  port_no       = each.value["port_no"]
 }
