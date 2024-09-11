a11e2320006a409bc3fadc46585bb04be012219c
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-11 14:51:38 -0500

    Auto push

--- module/data.tf
+++ module/data.tf
@@ -5,7 +5,7 @@ data "aws_route53_zone" "main" {
 
 data "aws_ami" "main" {
   most_recent = true
-  name_regex  = "DevOps-LabImage-RHEL9"
-  #name_regex = "b58-golden-image"
-  owners     = ["355449129696"]
+  #name_regex  = "DevOps-LabImage-RHEL9"
+  name_regex = "b58-golden-image"
+  owners     = ["011528299590"]
 }
