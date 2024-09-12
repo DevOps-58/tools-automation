892bb5f9e977cf2678ab61501104cf15a9ec243a
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-12 13:52:49 -0500

    Auto push

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "tools" {
     default = {
         vault ={
-            name = "vault"
-            instance_type = "t3.small"
-            port_no = 8200
+          name = "vault"
+          instance_type = "t3.small"
+          port_no = 8200
         }
     }
   
