a373e1d7008cf5bb43630efa10dccb2e5b9b6524
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-16 13:39:15 -0500

    Auto push

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i ${tool_name}.expense.internal, -e ansible_user=ec2-user -e ansible_password=${SSH_PSW} -e tool_name=${tool_name}  setup-tools.yml
+ansible-playbook -i ${tool_name}.expense.internal, -e ansible_user=ec2-user -e ansible_password=${ssh_pwd} -e tool_name=${tool_name}  setup-tools.yml
