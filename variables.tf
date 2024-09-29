bac221f4f2f7c105001e441ee54e8dcd826c28e6
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-28 22:19:48 -0500

    Auto push

--- variables.tf
+++ variables.tf
@@ -1,23 +1,30 @@
 variable "tools" {
-    default = {
-        vault = {
-          name = "vault"
-          instance_type = "t3.small"
-          port_no = 8200
-        }
-
-        prometheus = {
-           name          = "prometheus"
-           instance_type = "t3.small"
-           port_no       = 9090
-        }
-
-        grafana = {
-           name          = "grafana"
-           instance_type = "t3.small"
-           port_no = 3000
-        }
-
+  default = {
+    vault = {
+      name          = "vault"
+      instance_type = "t3.small"
+      port_no = {
+        vault = 8200
+      }
     }
-  
+    prometheus = {
+      name          = "prometheus"
+      instance_type = "t3.small"
+
+      port_no = {
+        prometheus = 9090
+      }
+    }
+
+    grafana = {
+      name          = "grafana"
+      instance_type = "t3.small"
+
+      port_no = {
+        grafana = 3000
+      }
+    }
+
+
+  }
 }
